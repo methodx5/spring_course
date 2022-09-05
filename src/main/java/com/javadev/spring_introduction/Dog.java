eade6156dd7db4e5a9f13eb55898f4374831dac6
John Doe <methodvr3@gmail.com>
2022-09-05 19:05:34 +0300

    Study of spring day 2.1

--- src/main/java/com/javadev/spring_introduction/Dog.java
+++ src/main/java/com/javadev/spring_introduction/Dog.java
@@ -2,10 +2,21 @@ package com.javadev.spring_introduction;
 
 public class Dog implements Pet {
 
+    private String name;
+
+
     public Dog(){
         System.out.println("Dog bean is created");
     }
 
+
+    public void init(){
+        System.out.println("Class Dog: init method ");
+    }
+
+    public void destroy (){
+        System.out.println("Class Dog: destroy method ");
+    }
     @Override
     public void say (){
         System.out.println("Bow-Wow!");
---
+++ src/main/java/com/javadev/spring_introduction/Test4.java
@@ -0,0 +1,21 @@
+package com.javadev.spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test4 {
+
+    public static void main(String[] args) {
+//        ClassPathXmlApplicationContext context =
+//                new ClassPathXmlApplicationContext("applicationContext2.xml");
+//
+//        Dog myDog = context.getBean("myPet", Dog.class);
+//        myDog.setName("Tor");
+//        Dog yourDog = context.getBean("myPet", Dog.class);
+//        yourDog.setName("Loc");
+//        System.out.println(myDog==yourDog);
+//        System.out.println(myDog.getName() + " " + yourDog.getName());
+//
+//        context.close();
+
+    }
+}
