32d016b7f2fee453b7bf6f39e507d030a398a267
John Doe <methodvr3@gmail.com>
2022-09-11 18:17:15 +0300

    Study of spring (Hibernate_intro) day 6

---
+++ src/main/java/com/javadev/hibernate_test/Test1.java
@@ -0,0 +1,27 @@
+package com.javadev.hibernate_test;
+
+import com.javadev.hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            Employee emp = new Employee("Arthur", "Morgan", "IT", 500);
+            session.beginTransaction();
+            session.save(emp);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
+    }
+}
